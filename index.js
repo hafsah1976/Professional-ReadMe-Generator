57a4025535ed8521117f44e468025c3059e73daf
Khan-HB <hafsah999@gmail.com>
2023-07-20 22:54:20 -0400

    coded readme questions and 1 license function

--- index.js
+++ index.js
@@ -1,13 +1,115 @@
 // TODO: Include packages needed for this application
-
+const inquirer=require("inquirer"); //for user input
+const fs=require("fs");//for saving the read me 
+const generateMarkdown= require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+{
+    type:"input",
+    name:"projecttitle",
+    message:"Please enter the title of the Project:- ",
+},
+{
+    type:"input",
+    name:"description",
+    message:"Please give a short description of the project:- ",
+
+},
+
+{
+    type:"input",
+    name:"userstory",
+    message:"Please enter the user story of the project:- ",
+
+},
+{
+    type:"input",
+    name:"criteria",
+    message:"Please enter the acceptance criteria of the project:- ",
+},
+
+{
+    type:"input",
+    name:"install",
+    message:"Please provide instructions and commands for installation for the project:- ",
+},
+{
+    type:"input",
+    name:"deploy",
+    message:"Please enter your deployment URL:- ",
+},
+{
+    type:"input",
+    name:"usage",
+    message:"Please state the purpose the project:- ",
+
+},
+
+{
+    type:"list",
+    name:"tests",
+    message:"Please enter the test cases for the project:- ",
+},
+
+ {
+    type:"image",
+    name:"screenshot",
+    message:"Please Upload your screenshot(s).",
+ },
+
+{
+    type:"input",
+    name:"username",
+    message:"Please enter your name:- ",
+},
+{
+    type:"input",
+    name:"emailID",
+    message:"Please enter your E-mail address:- ",
+},
+
+{
+    type:"input",
+    name:"github",
+    message:"Please enter your GitHub Repo URL:- ",
+},
+{
+    type:"list",
+    choices:['GNU AGPLv3',
+    'GNU GPLv3', 
+    'GNU LGPLv3',
+    'Mozilla Public License 2.0', 
+    'Apache License 2.0', 
+    'MIT License',
+    'The Unlicense',
+    'Boost Software License 1.0' ],
+    name:"license",
+    message:"Please select a license for the project.",
+},
+
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,generateMarkdown(data), function(err){
+        if(err){
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data)=>{
+        console.log(JSON.stringify(data,null,""));
+        // data.renderLicenseBadge=renderLicenseBadge(data.license);
+        writeToFile("sampleREADME.md",data);
+
+    });
+      }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,162 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if (license === "GNU AGPLv3") {
+
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license === "GNU GPLv3") {
+
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "GNU LGPLv3") {
+
+    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license === "Mozilla Public License 2.0") {
+
+    return "[![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license === "MIT") {
+
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache License 2.0") {
+
+    return "[![License:Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "The Unlicense") {
+
+    return "[License: Unlicense](https://upload.wikimedia.org/wikipedia/commons/e/ee/Unlicense_Blue_Badge.svg)";
+  } else if (license === "Boost Software License 1.0") {
+
+    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else {
+    return `  No Badge Selected  `;
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+function renderLicenseLink(license) {
+
+  if (!license) {
+    return '';
+  }
+
+  return `
+  <a href="https://choosealicense.com/licenses/${license}">
+    ${license}
+  </a>
+  `;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+  ## License
+
+  This project is licensed under the ${license} license.
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+  return `# ${data.projecttitle}
+  
+  ${data.description}
+
+  ${data.license}
+
+  ...
+  
+  ## Table of Contents
+
+  - [User Story](#userstory)
+  - [Acceptance Criteria](#acceptance)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contibuting)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ...
+
+  ## User Story
+
+  ${data.userstory}
+  ...
+
+  ## Acceptance Criteria
+
+  ${data.criteria}
+  ...
+
+  ## Usage 
+
+    [Deployment URL](${data.deploy})
+
+  ${data.usage}
+  ...
+
+  ## Screenshot
+
+  // '{data.screenshot}'
+
+  ![]()
+  ...
+
+  ## Installation
+
+  The URL will mostly work in all browsers. 
+  The preferred Browser is Google Chrome and the extension Chrome DevTools Extension to view the source code. 
+  GitHub is a cloud service for projects/repositories. 
+  You can simply visit the URL to view the repository in your browser 
+
+  ${data.install}
+  ...
+
+  ## Contributing
+
+  Follow the steps below if you wish to contribute to this application:
+
+  - Fork the repository from github
+  - Create a new branch and switch to your branch using 'git checkout branchname'
+  - Commit your changes
+  - Push your branch using 'git push' to GitHub
+  - Create a new pull request
+  - Your code will get reviewed and your branch will be merged.
+  ...
+
+  ## Tests
+
+  ${data.test}
+  ...
+
+  ## Questions
+
+  My name is ${data.username}. ${'\n'} If you have any questions about the repo, open an issue or contact me directly at ${data.emailID}. ${'\n'}You can find more of my work at ${data.github}
+
+  Thank you for reading till the end.
+  ...
+
+  ## License
+
+  ${data.license}
+  ...
+
+    ${renderLicenseBadge(data.license)}
+     ...
+
+    ${renderLicenseLink(data.license)}
+    ...
+
+    ${renderLicenseSection(data.license)}
+    ...
+  
+    `;
+};
 
 module.exports = generateMarkdown;
